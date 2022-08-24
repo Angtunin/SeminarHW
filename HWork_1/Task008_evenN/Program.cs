95acb6b5decf076263e24069539d5eb525a259a4
AleksandrTunin <angtunin@mail.ru>
2022-08-24 16:28:03 +0800

    add HWork_1

---
+++ HWork_1/Task008_evenN/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number_a = int.Parse(Console.ReadLine());
+int count = 2;
+
+while (count < number_a)
+{
+    Console.Write($"{count } ");
+    count +=2;
+}
---
+++ HWork_1/Task4_max3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три 
+//           числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число a: ");
+int number_a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int number_b = int.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+int number_c = int.Parse(Console.ReadLine());
+int max = number_a;
+
+if (number_a > max) max = number_a;
+if (number_b > max) max = number_b;
+if (number_c > max) max = number_c;
+
+Console.Write("Max = {0}", max);
