ebe0e78773c783e608c54d729b6811500276fe92
AleksandrTunin <angtunin@mail.ru>
2022-09-22 21:28:58 +0800

    add HWork_5

---
+++ HWork_5/Task038/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+double[] ArrayDouble(int size)
+{
+    double[] array = new double[size];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(-100, 100) + rand.NextDouble();    
+    }
+    return array;    
+}
+double NumMaxArray(double[] array)
+{
+    double NumMax = 0;
+    foreach (var el in array)
+    {
+        if (el > NumMax) NumMax = el;
+    }
+    return NumMax;
+}
+
+double NumMinArray(double[] array)
+{
+    double NumMin = 0;
+    foreach (var el in array)
+    {
+        if (el < NumMin) NumMin = el;
+    }
+    return NumMin;
+}
+
+Console.Write("Введите размерность массива: ");
+int Num = int.Parse(Console.ReadLine());
+double[] myArray = ArrayDouble(Num);
+Console.WriteLine(String.Join(" ", myArray));
+Console.WriteLine($"{NumMaxArray(myArray) - NumMinArray(myArray)}");
---
+++ HWork_5/TaskHard/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать метод сортировки массива "пузырьковый" способом,
+// реализовать невозрастающую сортировку.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] BableSort(int[] array)
+{
+    int size = array.Length;
+    int temp = 0;
+    for (int i = 0; i < size; i++)
+    {
+        
+        for (int k = 0; k < size - 1 - i; k++)
+        {
+            if (array[k] > array[k + 1])
+            {
+                temp = array[k + 1];
+                array[k + 1] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+    return array; 
+}
+
+int[] myArray = GetArray(10, -10, 10);
+
+Console.WriteLine(String.Join(" ", myArray));
+int[] Result = BableSort(myArray);
+Console.WriteLine(String.Join(" ", Result));
