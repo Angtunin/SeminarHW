25440f6d7335e951d138fdfe980da08f3e3033e3
AleksandrTunin <angtunin@mail.ru>
2022-09-25 20:13:39 +0800

    add HWork_6

---
+++ HWork_6/Task041/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+// void PlusNum(char[] arr, int Num)
+// {
+    
+//     int plusN = 0;
+//     int item = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (Convert.ToInt32(arr[i]) > 0)
+//         {
+//             plusN++;
+//             item = item + arr[i];
+//             Console.WriteLine(arr[i]);
+//         } 
+
+//     }
+//     Console.WriteLine(plusN);
+// }
+
+// string InputN(int Num)
+// {
+//     // int M = int.Parse(Console.ReadLine());
+//     int i = 0;
+//     int count = 0;
+//     string arr = String.Empty;
+//     while (i < Num)
+//     {
+//         Console.Write($"Введите число {i + 1}: ");
+//         int Dm = int.Parse(Console.ReadLine());
+//         if (Dm > 0) count++;
+//         arr = arr + Dm.ToString();
+//         i++; 
+//     }
+//     return arr; count.ToString();
+
+// }
+
+Console.Write("Введите число M (количество чисел): ");
+
+int M = int.Parse(Console.ReadLine());
+int i = 0;
+int count = 0;
+string arr = String.Empty;
+while (i < M)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int Dm = int.Parse(Console.ReadLine());
+    if (Dm > 0) count++;
+    arr = arr + Dm.ToString() + " ";
+    i++; 
+}
+
+Console.WriteLine($"[{String.Join(" ", arr.ToCharArray())}] количество положительных чисел {count}");
+
+// var result = InputN(M);
+// Console.WriteLine(result);
+// char[] myArray = result.ToArray();
+
+
+// PlusNum(myArray, M);
+// char[] myArray = arr.ToCharArray();
+
+// Console.WriteLine($"количество чётных чисел = {String.Join(" ", PlusNum(myArray))}");
+
---
+++ HWork_6/Task043/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[,] InputNum(int K, int B)
+{
+    double[,] matrix = new double[K, B];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                Console.Write($"Введите коэф b{j + 1}: ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            } 
+            else
+            {
+                Console.Write($"Введите коэф k{j + 1}: ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            } 
+        }    
+    }
+    return matrix;
+}
+
+void Equation(double[,] array)
+{
+    double[] res = new double[2];
+    res[0] = (array[0, 1] - array[0, 0]) / (array[1, 0] - array[1, 1]);
+    res[1] = array[1, 0] * res[0] + array[0, 0];
+    Console.WriteLine($"Точка пересечения: ({res[0]}, {res[1]})");
+}
+
+double[,] myArray = InputNum(2, 2);
+void printArray(double[,] arr)
+{
+   for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }    
+}
+printArray(myArray);
+Equation(myArray);
