20653d8f56df193feeaef8891712af06f129e00f
AleksandrTunin <angtunin@mail.ru>
2022-10-04 16:17:13 +0800

    add HWork_9

---
+++ HWork_9/Task066/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumber(int m, int n)
+{
+    
+    if (m == n)
+    {
+        return m;
+    }
+    return m + SumNumber(m + 1, n);
+}
+
+Console.Write("Введите число m: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNumber(numberM, numberN));
